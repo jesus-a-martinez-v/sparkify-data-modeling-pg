cd59ffca2e85bbc38eb0b5258b404e77fade1e4e
Jesus Martinez <jesus@Jesuss-MacBook-Pro.local>
2020-03-18 10:51:23 -0500

    Partial implementation.

--- sql_queries.py
+++ sql_queries.py
@@ -1,52 +1,102 @@
 # DROP TABLES
 
-songplay_table_drop = ""
-user_table_drop = ""
-song_table_drop = ""
-artist_table_drop = ""
-time_table_drop = ""
+songplay_table_drop = "DROP TABLE IF EXISTS songplay"
+user_table_drop = "DROP TABLE IF EXISTS users"
+song_table_drop = "DROP TABLE IF EXISTS songs"
+artist_table_drop = "DROP TABLE IF EXISTS artists"
+time_table_drop = "DROP TABLE IF EXISTS times"
 
 # CREATE TABLES
 
 songplay_table_create = ("""
+CREATE TABLE IF NOT EXISTS songplays(
+    songplay_id SERIAL PRIMARY KEY,
+    start_time BIGINT REFERENCES time(start_time),
+    user_id VARCHAR(100) REFERENCES users(user_id),
+    level VARCHAR(50),
+    song_id VARCHAR(100) REFERENCES songs(song_id),
+    artist_id VARCHAR(100) REFERENCES artists(artist_id),
+    session_id INT,
+    location VARCHAR(200),
+    user_agent VARCHAR(200)
 """)
 
 user_table_create = ("""
+CREATE TABLE IF NOT EXISTS users(
+    user_id VARCHAR(100) PRIMARY KEY,
+    first_name VARCHAR(100) NOT NULL,
+    last_name VARCHAR(100) NOT NULL,
+    gender CHAR(1) NOT NULL,
+    level VARCHAR(50) NOT NULL
+)
+""")
+artist_table_create = ("""
+CREATE TABLE IF NOT EXISTS artists(
+    artist_id VARCHAR(100) PRIMARY KEY,
+    name VARCHAR(200) NOT NULL,
+    location VARCHAR(200) NOT NULL,
+    latitude FLOAT,
+    longitude FLOAT
+)
 """)
 
 song_table_create = ("""
-""")
-
-artist_table_create = ("""
+CREATE TABLE IF NOT EXISTS songs(
+    song_id VARCHAR(100) PRIMARY KEY,
+    title VARCHAR(200) NOT NULL,
+    artist_id VARCHAR(100) NOT NULL,
+    year INT NOT NULL,
+    duration FLOAT NOT NULL
+)
 """)
 
 time_table_create = ("""
+CREATE TABLE IF NOT EXISTS time(
+    start_time BIGINT PRIMARY KEY,
+    hour INT NOT NULL,
+    day INT NOT NULL,
+    week INT NOT NULL,
+    month INT NOT NULL,
+    year INT NOT NULL,
+    weekday INT NOT NULL
+)
 """)
 
 # INSERT RECORDS
 
 songplay_table_insert = ("""
+INSERT INTO songplays(start_time, user_id, level, song_id, artist_id, session_id, location, user_agent) VALUES (%s, %s, %s, %s, %s, %s, %s, %s)
 """)
 
 user_table_insert = ("""
+INSERT INTO users(user_id, first_name, last_name, gender, level) VALUES (%s, %s, %s, %s, %s) ON CONFLICT DO NOTHING
 """)
 
 song_table_insert = ("""
+INSERT INTO songs(song_id, title, artist_id, year, duration) VALUES (%s, %s, %s, %s, %s)
 """)
 
 artist_table_insert = ("""
+INSERT INTO artists(artist_id, name, location, latitude, longitude) VALUES (%s, %s, %s, %s, %s) ON CONFLICT DO NOTHING
 """)
 
-
 time_table_insert = ("""
+INSERT INTO time(start_time, hour, day, week, month, year, weekday) VALUES (%s, %s, %s, %s, %s, %s, %s) ON CONFLICT DO NOTHING
 """)
 
 # FIND SONGS
 
 song_select = ("""
+SELECT songs.song_id, songs.artist_id
+FROM songs, artists
+WHERE songs.artist_id = artists.artist_id
+ AND songs.title = %s
+ AND artists.name = %s
+ AND songs.duration = %s
 """)
 
 # QUERY LISTS
 
-create_table_queries = [songplay_table_create, user_table_create, song_table_create, artist_table_create, time_table_create]
-drop_table_queries = [songplay_table_drop, user_table_drop, song_table_drop, artist_table_drop, time_table_drop]
+create_table_queries = [user_table_create, artist_table_create, song_table_create, time_table_create,
+                        songplay_table_create]
+drop_table_queries = [songplay_table_drop, artist_table_drop, user_table_drop, song_table_drop, time_table_drop]
