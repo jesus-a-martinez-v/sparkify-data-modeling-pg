2a23e34e555dd75cc99088c05283e8ffb499e5bb
Jesus Martinez <jesus@Jesuss-MacBook-Pro.local>
2020-03-19 15:14:28 -0500

    Conflictive upserts into the user table updates the level.

--- sql_queries.py
+++ sql_queries.py
@@ -82,7 +82,7 @@ INSERT INTO songplays(start_time, user_id, level, song_id, artist_id, session_id
 user_table_insert = ('''
 INSERT INTO users(user_id, first_name, last_name, gender, level) 
           VALUES (%s, %s, %s, %s, %s) 
-          ON CONFLICT DO NOTHING
+          ON CONFLICT (user_id) DO UPDATE SET level = EXCLUDED.level
 ''')
 
 song_table_insert = ('''
